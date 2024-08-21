a353f419d6afb0dc8ae49c78b776ad33088993ab
PedrOliveiraM <pedropucmont@gmail.com>
2024-08-21 18:13:03 -0300

    chore: updated setup with imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+// Imports PRIMENG
+import { ButtonModule } from 'primeng/button';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { LoginComponent } from './components/auth/login/login.component';
+import { CheckboxModule } from 'primeng/checkbox';
+import { AppRoutingModule } from './app.routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { InputTextModule } from 'primeng/inputtext';
+
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon'; // Importe o MatIconModule
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { NgIf } from '@angular/common';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    ForgotPasswordComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    TabMenuModule,
+    CheckboxModule,
+    AppRoutingModule,
+    MatSlideToggleModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    InputTextModule,
+    NgIf,
   ],
   providers: [],
   bootstrap: [AppComponent]
