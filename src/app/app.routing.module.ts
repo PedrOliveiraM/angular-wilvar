c9998f1b9833c9399d6741b4778443fabba182b4
PedrOliveiraM <pedropucmont@gmail.com>
2024-08-21 18:12:31 -0300

    feat: add routes config

---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/auth/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { ForgotPasswordComponent } from './components/auth/forgot-password/forgot-password.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'register', component: RegisterComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
